df3ca2c60c1dd794031b95d4cc703945ab5dbdad
yuxino <nbsawz@gmail.com>
2026-08-11 15:11:17 +0800

    fix: add pm2 server entrypoint

---
+++ src/server/start.js
@@ -0,0 +1,6 @@
+import { createServer } from "./index.js";
+
+const { server, config } = createServer();
+server.listen(config.port, "127.0.0.1", () => {
+  console.log(`[koe] listening on http://127.0.0.1:${config.port} (${config.provider})`);
+});
